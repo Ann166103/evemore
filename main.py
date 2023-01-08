c3247677882df344ed4e1b2a72569f2385763e86
Ann <ankakazaeva@yandex.ru>
2023-01-09 01:24:26 +0300

    price and spikes

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ STEP = 50
 size = WIDTH, HEIGHT = 700, 550
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+price = 0
 
 
 def load_image(name, colorkey=None):
@@ -38,22 +39,30 @@ def terminate():
 
 def start_screen():  # Заставка
     intro_text = ["ЭВЕМОР", "",
+                  "Проберитесь в непреступный",
+                  "замок Эвемор.",
+                  "",
                   "Дойдите до лестницы,",
                   "чтобы подняться на",
                   "следующий этаж"]
 
-    fon = pygame.transform.scale(load_image('fon3.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 50
+    text_coord = 20
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, 1, pygame.Color(255, 255, 255))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        if line == "ЭВЕМОР":
+            intro_rect.x = 315
+        else:
+            intro_rect.x = 200
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+        if line == "ЭВЕМОР":
+            text_coord += 70
+        text_coord += 10
 
     while True:
         for event in pygame.event.get():
@@ -66,23 +75,51 @@ def start_screen():  # Заставка
         clock.tick(FPS)
 
 
-def finish_screen():  # Заставка
-    intro_text = ["ЭВЕМОР", "",
-                  "GAME OVER",
-                  "Вы прошли 3 уровня игры"]
+def game_over_screen(price):
+    fon = pygame.transform.scale(load_image('fon4.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                finish_screen(price, False)
+                terminate()
+        pygame.display.flip()
+        clock.tick(FPS)
 
-    fon = pygame.transform.scale(load_image('fon3.jpg'), (WIDTH, HEIGHT))
+
+def finish_screen(price, f):
+    if f:
+        intro_text = ["ЭВЕМОР", "",
+                      "Поздравляем!",
+                      "Вы смогли пробраться",
+                      "в непреступный замок Эвемор",
+                      f"И заработали {price} монет."]
+    else:
+        intro_text = ["ЭВЕМОР", "",
+                      "Вы не смогли пробраться",
+                      "в непреступный замок Эвемор",
+                      f"Попробуйте еще раз."]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, 1, pygame.Color(255, 255, 255))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        if line == "ЭВЕМОР":
+            intro_rect.x = 315
+        else:
+            intro_rect.x = 200
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+        if line == "ЭВЕМОР":
+            text_coord += 70
+        text_coord += 10
 
     while True:
         for event in pygame.event.get():
@@ -111,18 +148,19 @@ def load_level(filename):
 
 tile_images = {
     'wall': load_image('wall3.png'),
-    'empty': load_image('ice.png'),
-    'lest': load_image('lest2.png')
+    'enemies': load_image('enemis.png'),
+    'lest': load_image('lest5.png'),
+    'price': load_image('price2.png')
 }
 player_image = load_image('hero50.png')
 
 tile_width = tile_height = 50
 
 
-class Tile(pygame.sprite.Sprite):  # пустое пространство
+class Price(pygame.sprite.Sprite):  # пустое пространство
 
     def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
+        super().__init__(price_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
@@ -137,10 +175,19 @@ class Wall(pygame.sprite.Sprite):  # стены
             tile_width * pos_x, tile_height * pos_y)
 
 
-class Lest(pygame.sprite.Sprite):  # стены
+class Lest(pygame.sprite.Sprite):  # лестница
 
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(lest_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Enemies(pygame.sprite.Sprite):  # враги
+
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(enemies_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
@@ -158,26 +205,30 @@ class Player(pygame.sprite.Sprite):
 player = None
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
+price_group = pygame.sprite.Group()
 wall_group = pygame.sprite.Group()
 lest_group = pygame.sprite.Group()
+enemies_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+
 
 
 def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
+            if level[y][x] == '*':
+                Price('price', x, y)
             elif level[y][x] == '#':
                 Wall('wall', x, y)
             elif level[y][x] == '@':
-                Tile('empty', x, y)
+                # Tile('empty', x, y)
                 new_player = Player(x, y)
             elif level[y][x] == '&':
                 Lest('lest', x, y)
-                print(x, y)
+            elif level[y][x] == '%':
+                Enemies('enemies', x, y)
+                # AnimatedSprite(load_image("dragon8x2.png"), 8, 2, x, y)
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
@@ -185,111 +236,88 @@ def generate_level(level):
 start_screen()
 
 
-def osnov(player, event):
-            if event.key == pygame.K_LEFT:
-                player.rect.x -= STEP
-                if player.rect.x < 0:
-                    player.rect.x = 0
-                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
-                    player.rect.x += STEP
-            if event.key == pygame.K_RIGHT:
-                player.rect.x += STEP
-                if player.rect.x > WIDTH - STEP:
-                    player.rect.x = WIDTH - STEP
-                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+def osnov(player, price):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
                     player.rect.x -= STEP
-            if event.key == pygame.K_UP:
-                player.rect.y -= STEP
-                if player.rect.y < 0:
-                    player.rect.y = 0
-                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
-                    player.rect.y += STEP
-            if event.key == pygame.K_DOWN:
-                player.rect.y += STEP
-                if player.rect.y > HEIGHT - STEP:
-                    player.rect.y = HEIGHT - STEP
-                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                    if player.rect.x < 0:
+                        player.rect.x = 0
+                    if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                        player.rect.x += STEP
+                if event.key == pygame.K_RIGHT:
+                    player.rect.x += STEP
+                    if player.rect.x > WIDTH - STEP:
+                        player.rect.x = WIDTH - STEP
+                    if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                        player.rect.x -= STEP
+                if event.key == pygame.K_UP:
                     player.rect.y -= STEP
+                    if player.rect.y < 0:
+                        player.rect.y = 0
+                    if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                        player.rect.y += STEP
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += STEP
+                    if player.rect.y > HEIGHT - STEP:
+                        player.rect.y = HEIGHT - STEP
+                    if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                        player.rect.y -= STEP
+        if pygame.sprite.groupcollide(player_group, price_group, False, True):
+            price += 10
+        if pygame.sprite.groupcollide(player_group, enemies_group, True, False):
+            return (1, price)
+        if not pygame.sprite.groupcollide(player_group, lest_group, False, False):
+            screen.fill(pygame.Color(25, 25, 112))
+            price_group.draw(screen)
+            wall_group.draw(screen)
+            lest_group.draw(screen)
+            enemies_group.draw(screen)
+            player_group.draw(screen)
+        else:
+            price_group.empty()
+            wall_group.empty()
+            lest_group.empty()
+            enemies_group.empty()
+            player_group.empty()
+            return (2, price)
+
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
-def level1():
+def level1(price):
     player, level_x, level_y = generate_level(load_level('level1.txt'))
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                osnov(player, event)
-        if not pygame.sprite.groupcollide(player_group, lest_group, False, True):
-            screen.fill(pygame.Color(0, 0, 0))
-            tiles_group.draw(screen)
-            wall_group.draw(screen)
-            lest_group.draw(screen)
-            player_group.draw(screen)
-        else:
-            tiles_group.empty()
-            wall_group.empty()
-            lest_group.empty()
-            player_group.empty()
-            level2()
-
-        pygame.display.flip()
-        clock.tick(FPS)
+    a = osnov(player, price)
+    if a[0] == 1:
+        game_over_screen(a[1])
+    if a[0] == 2:
+        level2(a[1])
     terminate()
 
 
-def level2():
+def level2(price):
     player, level_x, level_y = generate_level(load_level('level2.txt'))
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                osnov(player, event)
-        if not pygame.sprite.groupcollide(player_group, lest_group, False, False):
-            screen.fill(pygame.Color(0, 0, 0))
-            tiles_group.draw(screen)
-            wall_group.draw(screen)
-            lest_group.draw(screen)
-            player_group.draw(screen)
-        else:
-            tiles_group.empty()
-            wall_group.empty()
-            lest_group.empty()
-            player_group.empty()
-            level3()
-
-        pygame.display.flip()
-        clock.tick(FPS)
+    a = osnov(player, price)
+    if a[0] == 1:
+        game_over_screen(a[1])
+    if a[0] == 2:
+        level3(a[1])
     terminate()
 
 
-def level3():
+def level3(price):
     player, level_x, level_y = generate_level(load_level('level3.txt'))
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                osnov(player, event)
-        if not pygame.sprite.groupcollide(player_group, lest_group, False, False):
-            screen.fill(pygame.Color(0, 0, 0))
-            tiles_group.draw(screen)
-            wall_group.draw(screen)
-            lest_group.draw(screen)
-            player_group.draw(screen)
-        else:
-            tiles_group.empty()
-            wall_group.empty()
-            lest_group.empty()
-            player_group.empty()
-            finish_screen()
-
-        pygame.display.flip()
-        clock.tick(FPS)
+    a = osnov(player, price)
+    if a[0] == 1:
+        game_over_screen(a[1])
+    if a[0] == 2:
+        finish_screen(a[1], True)
     terminate()
 
-level1()
+
+level1(price)
